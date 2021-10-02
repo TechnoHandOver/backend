47784ceff69924f3acc552ad3877843473534079
Nikita Lobaev <lobaevni@yandex-team.ru>
2021-10-02 17:32:51 +0300

    Add basic database migration script

---
+++ database.sql
@@ -0,0 +1,22 @@
+\c handover;
+
+DROP SCHEMA handover.public CASCADE;
+CREATE SCHEMA handover.public;
+
+CREATE TABLE user_ (
+    id SERIAL PRIMARY KEY,
+    vk_id INT NOT NULL UNIQUE
+);
+
+CREATE TABLE ads (
+    id SERIAL PRIMARY KEY,
+    user_author_id INT NOT NULL REFERENCES user_ (id) ON DELETE CASCADE,
+    location_from TEXT NOT NULL,
+    location_to TEXT NOT NULL,
+    time_from TIMESTAMP NOT NULL,
+    time_to TIMESTAMP NOT NULL,
+    min_price INT NOT NULL CHECK (min_price >= 0),
+    comment TEXT NOT NULL
+);
+
+CREATE INDEX ON ads (user_author_id);
